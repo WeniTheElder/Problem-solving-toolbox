5acd84484f7046df4f87f2646feef1845e7c5493
Ahmad <Ahmedabdellatef3846@gmail.com>
2025-03-05 01:59:39 +0200

    add (primeFactorization) function

---
+++ Implementatnios/primeFactorization.cpp
@@ -0,0 +1,23 @@
+int factors[100], exponent[100], length;
+
+void primeFactorization(int number){
+    int current_number = 2;
+    while(number > 1 && current_number <= sqrt(number)){
+        int repetitionCounter = 0;
+        while(number% current_number == 0){
+            number /= current_number;
+            ++repetitionCounter;
+        }
+        if(repetitionCounter > 0){
+            factors[length] = current_number;
+            exponent[length] = repetitionCounter;
+            length++;
+        }
+        ++current_number;
+    }
+    if(number > 1){//prime
+        factors[length] = number;
+        exponent[length] = 1;
+        length++;
+    }
+}
