baff51477ca0ec768c4ad5a5b5a48350c9efe597
Ahmad <Ahmedabdellatef3846@gmail.com>
2025-03-05 22:29:06 +0200

    add fastModularExponentiation

---
+++ Implementations/fastMoudularExponentiation.cpp
@@ -0,0 +1,16 @@
+long long fastModularExponentiation(long long base,long long exponent,long long mod)
+{
+    long long ans = 1;
+    if (mod == 1) return 0;
+
+    while (exponent >0)
+    {
+        if (exponent %2 != 0)
+        {
+            ans = (ans *base)%mod;
+        }
+        base = (base*base)%mod;
+        exponent /= 2;
+    }
+    return ans;
+}
